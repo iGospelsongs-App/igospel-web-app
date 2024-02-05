9da1db95cdfbdff98d3b4de9094998e654954c41
omatsola619 <omarsunday0@gmail.com>
2024-02-05 19:09:32 +0100

    still working on sidebar, adjusted background and icons

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import searchIcon from '../assets/images/search.svg'
+import notifIcon from '../assets/images/notif.svg'
+import dp from '../assets/images/dp.svg'
+
+function Header() {
+  return (
+    <div className='flex items-center justify-between pr-7'>
+        {/* search bar */}
+        <div className='flex-1'>
+            <div className='flex max-w-[300px] items-center border-[1px] border-[#98A2B3] bg-white bg-opacity-5 px-2 py-[5px] rounded-lg'>
+                <img src={searchIcon} alt="" className='w-[14px] mr-1' />
+                <input type="text" className='outline-none text-xs w-full font-sf-reg bg-transparent text-white' placeholder='Search songs, albums, artists, sermons' />
+            </div>
+        </div>
+
+        <div className='flex items-center gap-5'>
+            {/* notification icon    */}
+            <div>
+                <img src={notifIcon} alt="" className='w-8' />
+            </div>
+            {/* profile icon  */}
+            <div>
+                <img src={dp} alt="" className='w-8' /> 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -5,6 +5,7 @@ import Sidebar from '../components/sidebar/Sidebar';
 import hamburger from '../assets/images/hamburger.svg'
 import Logo from '../assets/images/logo.svg'
 import SidebarClosed from '../components/sidebar/SidebarClosed';
+import Header from '../components/Header';
 
 function HomeLayout() {
     const sidebarIsOpen = useContext(SidebarContext);
@@ -17,7 +18,7 @@ function HomeLayout() {
     return (
         <div className='bg-gradient-to-br from-[#291317] via-transparent to-black'>
             <div className='flex items-start'>
-                <div className={`w-[270px] ${!sidebarIsOpen.isOpen && 'bg-transparent'} h-[100vh] fixed overflow-auto bg-black text-white`}>
+                <div className={`w-[220px] ${!sidebarIsOpen.isOpen && 'bg-transparent'} h-[100vh] fixed overflow-auto bg-black text-white`}>
                     <div className='pl-[30px] pt-2 flex items-center gap-4'>
                         <img src={hamburger} alt="" className='w-[27px] h-[27px] cursor-pointer' onClick={handleOpenSidebar} />
                         <img src={Logo} alt="" className='w-[110px] h-[50px]' />
@@ -30,8 +31,11 @@ function HomeLayout() {
                 </div>
 
 
-                <div className='w-full h-screen ml-[270px]'>
-                    {/* header here  */}
+                <div className={`w-full h-screen ml-[220px] ${isOpen && 'pl-7'}`}>
+                    <div className={`mt-5 mb-7`}>
+                        <Header />
+                    </div>
+
                     <Outlet />
                 </div>
             </div>
