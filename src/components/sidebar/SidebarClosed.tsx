f7ac4a916afd10378cccb80e622258ffa376dea0
omatsola619 <omarsunday0@gmail.com>
2024-02-19 14:09:04 +0100

    fixing sidebar closed issues

--- src/components/sidebar/SidebarClosed.tsx
+++ src/components/sidebar/SidebarClosed.tsx
@@ -10,7 +10,7 @@ function SidebarClosed() {
             <ul className="">
                 {
                     sidebarMenuData.map((item: MenuItem, i: number) => (
-                        <div key={i}>
+                        <div key={i} className='w-full'>
                             <NavLink to={item.src} className={`flex flex-col w-[90px] rounded-md py-2 pr-10 pl-[25px] items-center justify-center cursor-pointer text-white text-sm`}>
                                 <img alt="" src={item.icon} className='w-6 h-6' />
                                 <span className="flex-1 font-sf-reg text-[11px] text-center">{item.title}</span>
