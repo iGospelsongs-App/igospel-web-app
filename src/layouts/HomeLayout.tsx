332b4c71ef22863f9fd9b839dbc99bc8ea7df51e
omatsola619 <omarsunday0@gmail.com>
2024-02-19 14:16:19 +0100

    removing unnecessary comments

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -13,17 +13,6 @@ function HomeLayout() {
 
     return (
         <div className='bg-gradient-to-br from-[#291317] via-transparent to-black'>
-            {/* <div className=''>
-                <div className={`w-full overflow-x-hidden relative ${!isOpen && isMobile ? 'ml-[80px]' : (isOpen && isMobile) ? 'ml-[52px]' : 'ml-[220px]'} ${isOpen && 'pl-0'}`}>
-                    <div className={`mb-4 ${isScroll && 'bg-black'} sticky top-[0px] pt-5 pb-5 w-full ${isOpen && 'pl-7'}`}>
-                        <Header />
-                    </div>
-                    <div className={`h-[800px] ${isOpen && 'pl-7'} pr-7`}>
-                      <Outlet />  
-                    </div>
-                </div>
-            </div> */}
-
             <div>
                 {/* header  */}
                 <div className='fixed right-0 w-full top-0 z-10'>
